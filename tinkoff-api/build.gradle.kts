565298c22459c20c2e35512fba4d86ecb82a9e19
Evgenii <vorobeievgeniy@gmail.com>
2020-09-26 21:32:49 +0300

    xx

--- tinkoff-api/build.gradle.kts
+++ tinkoff-api/build.gradle.kts
@@ -1,14 +1,23 @@
 plugins {
     kotlin("jvm")
+    kotlin("plugin.serialization") version Versions.kotlin
 }
 
 dependencies {
 
     implementation(kotlin("stdlib"))
 
-    implementation(Libs.moshi_kotlin)
-    implementation(Libs.moshi_adapters)
-    implementation(Libs.threetenbp)
+    implementation(Libs.annotation)
+    implementation(Libs.coroutines_core)
+    implementation(Libs.kotlin)
     implementation(Libs.okhttp)
+    implementation(Libs.okhttp3_logging_interceptor)
+    implementation(Libs.okhttp_url_connection)
+    implementation(Libs.retrofit)
+    implementation(Libs.retrofit_kotlin_serialisation_converter)
+    implementation(group = "com.google.guava", name = "guava", version = "29.0-jre")
+
+    testImplementation(Libs.junit)
+    testImplementation(Libs.truth)
     testImplementation(Libs.kotlintest)
 }
